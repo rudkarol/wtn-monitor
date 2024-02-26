e1e3e497e26351abc765e85cc2ba49e9a56eaa89
LorrEkk <63709739+LorrEkk@users.noreply.github.com>
2024-02-26 20:31:35 +0100

    read and write past offers file

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ import settings
 from proxy import Load_proxies, Rotate_proxy
 
 
+def read_past_offers():
+    try:
+        with open('offers_IDs', 'rb') as offers_file:
+            offers_dict = pickle.load(offers_file)
+            print('offers_list: ', offers_dict)
+
+            return offers_dict
+    except FileNotFoundError:
+        with open('offers_IDs', 'wb'):
+            pass
+
+
+def save_offers(offers_dict):
+    try:
+        with open('offers_IDs', 'wb') as file:
+            pickle.dump(offers_dict, file)
+    except Exception:
+        pass
+
+
 session = tls_client.Session(client_identifier="chrome_120", random_tls_extension_order=True)
 
 proxies = Load_proxies()
