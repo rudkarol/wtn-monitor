da2ba0e65cc48defede615e80b1abba086db331d
LorrEkk <63709739+LorrEkk@users.noreply.github.com>
2024-02-26 19:53:12 +0100

    small code improvements

--- proxy.py
+++ proxy.py
@@ -13,25 +13,25 @@ class Proxy:
         }
 
 
-def Load_proxies() -> cycle:
-    proxy_list = []
-
+def Load_proxies():
     try:
         with open("proxies.txt", "r") as proxies_file:
+            proxy_list = []
+
             for line in proxies_file:
                 host, port, username, password = line.strip().split(":")
-                proxy_obj = Proxy(host, port, username, password)
-                proxy_list.append(proxy_obj)
-    except EOFError:
-        sys.exit("ERROR - proxies.txt file is empty!")
+                proxy_list.append(
+                    Proxy(host, port, username, password)
+                )
+
+            return cycle(proxy_list)
     except FileNotFoundError:
-        proxies_file = open("proxies.txt", "w")
-        proxies_file.close()
+        with open("proxies.txt", "w"):
+            pass
+
         sys.exit("ERROR - proxies.txt file does not exist! File created")
     except Exception as e:
         sys.exit("ERROR - " + str(e))
-
-    return cycle(proxy_list)
 
 
 def Rotate_proxy(session, proxies):
