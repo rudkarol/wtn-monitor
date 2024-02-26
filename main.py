66de01b41ad1298130d3de607f8752aeed1dc9a7
LorrEkk <63709739+LorrEkk@users.noreply.github.com>
2024-02-26 21:00:48 +0100

    read acceptable offers

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import tls_client
+import csv
+
 import settings
+import cookies
+import headers
 from proxy import Load_proxies, Rotate_proxy
 
 
@@ -12,6 +17,8 @@ def read_past_offers():
     except FileNotFoundError:
         with open('offers_IDs', 'wb'):
             pass
+    except Exception:
+        return {}
 
 
 def save_offers(offers_dict):
@@ -22,7 +29,33 @@ def save_offers(offers_dict):
         pass
 
 
-session = tls_client.Session(client_identifier="chrome_120", random_tls_extension_order=True)
+def read_acceptable_offers():
+    try:
+        with open('wtn_acceptable.csv', 'r') as csvfile:
+            acceptable_dict = {}
+            reader = csv.DictReader(csvfile)
 
-proxies = Load_proxies()
-Rotate_proxy(session, proxies)
+            for row in reader:
+                acceptable_dict.update({int(row['PID']): int(row['MIN_PRICE'])})
+
+            print('acceptable_dict: ', acceptable_dict)
+
+            return acceptable_dict
+    except FileNotFoundError:
+        with open('wtn_acceptable.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['SKU', 'NAME', 'PID', 'MIN_PRICE'])
+            writer.writeheader()
+
+        sys.exit("ERROR - wtn_acceptable.csv file does not exist! File created")
+    except Exception as e:
+        sys.exit("ERROR - " + str(e))
+
+
+def main():
+    session = tls_client.Session(client_identifier="chrome_120", random_tls_extension_order=True)
+    proxies = Load_proxies()
+    Rotate_proxy(session, proxies)
+
+
+if __name__ == "__main__":
+    main()
