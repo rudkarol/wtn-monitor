1440f1232d42d639df6d78f6f0ba0101a3b2ae13
LorrEkk <63709739+LorrEkk@users.noreply.github.com>
2024-03-01 23:10:37 +0100

    review offers and accept offer request

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ import headers
 from proxy import Load_proxies, Rotate_proxy
 
 
-def read_past_offers():
-    try:
-        with open('offers_IDs', 'rb') as offers_file:
-            offers_dict = pickle.load(offers_file)
-            print('offers_list: ', offers_dict)
-
-            return offers_dict
-    except FileNotFoundError:
-        with open('offers_IDs', 'wb'):
-            return {}
-    except Exception:
-        return {}
+# def read_past_offers():
+#     try:
+#         with open('offers_IDs', 'rb') as offers_file:
+#             offers_dict = pickle.load(offers_file)
+#             print('offers_list: ', offers_dict)
+#
+#             return offers_dict
+#     except FileNotFoundError:
+#         with open('offers_IDs', 'wb'):
+#             return {}
+#     except Exception:
+#         return {}
 
 
 def save_offers(offers_dict):
@@ -31,7 +31,7 @@ def save_offers(offers_dict):
         pass
 
 
-def read_acceptable_offers():
+def read_acceptable_offers() -> dict:
     try:
         with open('wtn_acceptable.csv', 'r') as csvfile:
             acceptable_dict = {}
@@ -71,7 +71,39 @@ def initial_request(client: httpx.Client) -> str:
         raise KeyError('ERROR: accessToken is invalid - cookies are expired')
 
 
-def get_offers(client: httpx.Client, access_token: str):
+def review_offers(offers, acceptable_dict: dict, client: httpx.Client, access_token: str) -> None:
+    for offer in offers:
+        try:
+            if (offer['variantId'] in acceptable_dict) and (
+                    offer['price'] >= acceptable_dict[offer['variantId']]
+            ):
+                accept_offer(offer, client, access_token)
+        except KeyError:
+            pass
+
+
+def accept_offer(offer, client: httpx.Client, access_token: str) -> None:
+    # TODO: test
+    data = {
+        'name': offer['id'],
+        'status': "ACCEPTED",
+        'variantId': offer['variantId']
+    }
+
+    r = client.post(
+        'https://api-sell.wethenew.com/offers',
+        data=data,
+        headers=headers.accept_offer_header(access_token)
+    )
+
+    if r.status_code == httpx.codes.OK:
+        # accepted_webhook(offer)
+        print('Offer ', offer['id'], 'accepted - status code:', str(r.status_code))
+    else:
+        print('Failed to accept offer ', offer['id'], '- status code:', str(r.status_code))
+
+
+def get_offers(client: httpx.Client, access_token: str, acceptable: dict) -> None:
     r = (client.get('https://api-sell.wethenew.com/offers?take=10', headers=headers.get_offers_header(access_token))
          .raise_for_status()
          .json())
@@ -81,15 +113,15 @@ def get_offers(client: httpx.Client, access_token: str):
 
         cookies.save_cookies(client.cookies.jar)
 
-        return r['results']
+        review_offers(r['results'], acceptable, client, access_token)
     except KeyError:
         raise KeyError('ERROR: offers list is empty')
-    
+
 
 def main():
     with httpx.Client() as client:
         # proxies = Load_proxies()
-        # offers = read_past_offers()
+        acceptable_offers = read_acceptable_offers()
 
         # Rotate_proxy(session, proxies)
         client.cookies.update(cookies.restore_cookies())
@@ -100,13 +132,11 @@ def main():
             cookies.clear_cookies_file()
             sys.exit(e)
 
-
         try:
-            get_offers(client, access_token)
-        except Exception as e:
+            get_offers(client, access_token, acceptable_offers)
+        except (httpx.HTTPStatusError, KeyError) as e:
             print(e)
             cookies.clear_cookies_file()
-
 
 
 if __name__ == '__main__':
